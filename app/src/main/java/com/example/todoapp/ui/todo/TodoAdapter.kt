4937b9e5a6c4fdafacc9fe10d4eeef4f40608c44
jonathanduran7 <joniduran624@gmail.com>
2024-12-30 16:25:39 -0300

    feat(todo): create adapter

---
+++ app/src/main/java/com/example/todoapp/ui/todo/TodoAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.todoapp.ui.todo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.todoapp.R
+import com.example.todoapp.domain.ToDo
+
+class TodoAdapter: ListAdapter<ToDo, TodoViewHolder>(TodoDiffCallBack()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        return TodoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
