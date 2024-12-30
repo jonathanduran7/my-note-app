be6f39a3ed805b34e74bc4b46f4c810011bfe24c
jonathanduran7 <joniduran624@gmail.com>
2024-12-30 16:25:57 -0300

    feat(todo): add initial data

--- app/src/main/java/com/example/todoapp/ui/todo/TodoViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/todo/TodoViewModel.kt
@@ -1,7 +1,38 @@
 package com.example.todoapp.ui.todo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.todoapp.domain.ToDo
 
 class TodoViewModel : ViewModel() {
+
+    private val _todos = MutableLiveData<List<ToDo>>(
+        listOf(
+            ToDo(
+                id = 1,
+                title = "Buy milk",
+                isCompleted = true
+            ),
+            ToDo(
+                id = 2,
+                title = "Buy eggs",
+                isCompleted = false
+            ),
+            ToDo(
+                id = 3,
+                title = "Buy bread",
+                isCompleted = false
+            )
+        )
+    )
+    val todos: LiveData<List<ToDo>> = _todos
+
+    fun add(todo: ToDo) {
+        val current = _todos.value.orEmpty().toMutableList()
+        current.add(todo)
+        _todos.value = current
+    }
+
     // TODO: Implement the ViewModel
 }
