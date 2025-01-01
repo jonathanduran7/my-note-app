80daa879d643a74fca029bc1c3402075a0c8270e
jonathanduran7 <joniduran624@gmail.com>
2025-01-01 17:16:54 -0300

    feat: add data base

---
+++ app/src/main/java/com/example/todoapp/data/database/AppDataBase.kt
@@ -0,0 +1,11 @@
+package com.example.todoapp.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.todoapp.data.dao.TodoDao
+import com.example.todoapp.domain.ToDo
+
+@Database(entities = [ToDo::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDao(): TodoDao
+}
