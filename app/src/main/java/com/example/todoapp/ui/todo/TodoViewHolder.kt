fd167022bae605b0820bd289ac13d769a2015855
jonathanduran7 <joniduran624@gmail.com>
2025-01-03 15:55:04 -0300

    feat(todo): set listener in view holder

--- app/src/main/java/com/example/todoapp/ui/todo/TodoViewHolder.kt
+++ app/src/main/java/com/example/todoapp/ui/todo/TodoViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.todoapp.ui.todo
 
 import android.view.View
+import android.widget.Button
 import android.widget.CheckBox
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.R
@@ -8,8 +9,9 @@ import com.example.todoapp.domain.models.ToDo
 
 class TodoViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val checkBox: CheckBox = view.findViewById<CheckBox>(R.id.checkBox)
+    private val deleteButton: Button = view.findViewById<Button>(R.id.deleteButton)
 
-    fun bind(todo: ToDo, listener: OnTodoCheckListener) {
+    fun bind(todo: ToDo, listener: OnTodoCheckListener, deleteListener: OnTodoDelete) {
         checkBox.setOnCheckedChangeListener(null)
 
         checkBox.isChecked = todo.isCompleted
@@ -17,6 +19,10 @@ class TodoViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
         checkBox.setOnCheckedChangeListener { _, isChecked ->
             listener.onTodoCheckChanged(todo, isChecked)
+        }
+
+        deleteButton.setOnClickListener {
+            deleteListener.onTodoDelete(todo)
         }
     }
 }
