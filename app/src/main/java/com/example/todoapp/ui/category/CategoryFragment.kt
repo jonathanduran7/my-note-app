9b711193758ce6c5387c24b32d03b458c7b31b3b
jonathanduran7 <joniduran624@gmail.com>
2025-01-20 08:50:42 -0300

    feat(category): implement binding and recycler view

--- app/src/main/java/com/example/todoapp/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/category/CategoryFragment.kt
@@ -6,26 +6,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentCategoryBinding
 
 class CategoryFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CategoryFragment()
-    }
+    private var _binding: FragmentCategoryBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: CategoryViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private lateinit var categoryAdapter: CategoryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        categoryAdapter = CategoryAdapter()
+        binding.rvCategory.apply {
+            adapter = categoryAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
+
+        viewModel.categories.observe(viewLifecycleOwner) {
+            categoryAdapter.submitList(it)
+        }
     }
 }
