9eeffa1191cd615b313c4a9ce7390dcc891fce05
jonathanduran7 <joniduran624@gmail.com>
2025-01-20 08:50:23 -0300

    feat(category): add initial data in view model

--- app/src/main/java/com/example/todoapp/ui/category/CategoryViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/category/CategoryViewModel.kt
@@ -1,7 +1,22 @@
 package com.example.todoapp.ui.category
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.todoapp.domain.models.Category
+import com.example.todoapp.domain.models.ToDo
 
 class CategoryViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _categories = MutableLiveData<List<Category>>(emptyList())
+    val categories: LiveData<List<Category>> = _categories
+
+    init {
+        _categories.value = listOf(
+            Category(1, "Work"),
+            Category(2, "Personal"),
+            Category(3, "Shopping")
+        )
+    }
+
 }
