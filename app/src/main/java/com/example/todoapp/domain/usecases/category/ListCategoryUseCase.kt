f6f303172207df3eb31e482ab442bd0258dddce7
jonathanduran7 <joniduran624@gmail.com>
2025-01-25 17:39:24 -0300

    feat(category): modify list category use case

--- app/src/main/java/com/example/todoapp/domain/usecases/category/ListCategoryUseCase.kt
+++ app/src/main/java/com/example/todoapp/domain/usecases/category/ListCategoryUseCase.kt
@@ -1,9 +1,12 @@
 package com.example.todoapp.domain.usecases.category
 
 import com.example.todoapp.data.repository.CategoryRepository
+import com.example.todoapp.domain.models.Category
 
 class ListCategoryUseCase(
     private val categoryRepository: CategoryRepository
 ) {
-    suspend operator fun invoke() = categoryRepository.getAll()
+    suspend operator fun invoke(): List<Category> {
+        return categoryRepository.getAll()
+    }
 }
