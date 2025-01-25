2ab5d939db2e4f55b59bbaf8d378c8dc38d9ce2c
jonathanduran7 <joniduran624@gmail.com>
2025-01-25 17:39:10 -0300

    feat(category): injection in view modules

--- app/src/main/java/com/example/todoapp/system/injection/ViewModules.kt
+++ app/src/main/java/com/example/todoapp/system/injection/ViewModules.kt
@@ -1,12 +1,11 @@
 package com.example.todoapp.system.injection
 
-import androidx.room.Room
-import com.example.todoapp.data.database.AppDatabase
-import com.example.todoapp.data.repository.TodoRepository
+import com.example.todoapp.ui.category.CategoryViewModel
 import com.example.todoapp.ui.todo.TodoViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModels = module {
     viewModel { TodoViewModel(get(), get(), get(), get()) }
+    viewModel { CategoryViewModel(get(), get()) }
 }
