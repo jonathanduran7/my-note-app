3b81110aebb9eced68ec41d3949e623bc433b5a6
jonathanduran7 <joniduran624@gmail.com>
2025-01-25 17:07:57 -0300

    feat(category): implement entities and change version

--- app/src/main/java/com/example/todoapp/data/database/AppDataBase.kt
+++ app/src/main/java/com/example/todoapp/data/database/AppDataBase.kt
@@ -2,10 +2,13 @@ package com.example.todoapp.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.todoapp.data.dao.CategoryDao
 import com.example.todoapp.data.dao.TodoDao
+import com.example.todoapp.domain.models.Category
 import com.example.todoapp.domain.models.ToDo
 
-@Database(entities = [ToDo::class], version = 1, exportSchema = false)
+@Database(entities = [ToDo::class, Category::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao(): TodoDao
+    abstract fun categoryDao(): CategoryDao
 }
