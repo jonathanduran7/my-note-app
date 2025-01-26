0e4b847940adc54af4101799ba7c3b99ef07f42d
jonathanduran7 <joniduran624@gmail.com>
2025-01-26 10:38:37 -0300

    feat(category): open dialog for edit and save category edited

--- app/src/main/java/com/example/todoapp/ui/category/CategoryViewHolder.kt
+++ app/src/main/java/com/example/todoapp/ui/category/CategoryViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.todoapp.ui.category
 
+import android.app.Dialog
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -12,12 +13,48 @@ class CategoryViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val editBtn: Button = view.findViewById<Button>(R.id.updateCategoryBtn)
     private val categoryName: TextView = view.findViewById<TextView>(R.id.categoryName)
 
-    fun bind(category: Category, onDeleteListener: OnCategoryDelete){
+    fun bind(category: Category, onDeleteListener: OnCategoryDelete, onEditListener: OnCategoryEdit){
         categoryName.text = category.name
 
         deleteBtn.setOnClickListener {
             onDeleteListener.onCategoryDelete(category)
         }
-        //TODO: implement delete and edit button
+
+        editBtn.setOnClickListener {
+            setupCategoryDialog(
+                onEditListener,
+                category
+            )
+        }
+    }
+
+    private fun setupCategoryDialog(
+        onEditListener: OnCategoryEdit,
+        category: Category
+    ){
+        val dialog = Dialog(itemView.context)
+        dialog.setContentView(R.layout.dialog_category)
+        dialog.setCancelable(true)
+
+        dialog.findViewById<TextView>(R.id.textViewTitle).text = "Editar Categoria"
+        dialog.findViewById<Button>(R.id.buttonSaveCategory).text = "Editar"
+
+        val categoryName = dialog.findViewById<TextView>(R.id.editTextCategory)
+        categoryName.text = this.categoryName.text
+
+        if (categoryName.text.isEmpty()) {
+            return
+        }
+
+        dialog.findViewById<Button>(R.id.buttonSaveCategory).setOnClickListener {
+            val category = Category(
+                id = category.id,
+                name = categoryName.text.toString()
+            )
+            onEditListener.onCategoryEdit(category)
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
