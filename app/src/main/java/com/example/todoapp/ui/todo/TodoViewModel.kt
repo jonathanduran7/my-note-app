916e26b86b40cfcd2253ff3b38ca6bef72e67b40
jonathanduran7 <joniduran624@gmail.com>
2025-01-26 11:31:04 -0300

    feat(todo): get all categories for spinner in view model

--- app/src/main/java/com/example/todoapp/ui/todo/TodoViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/todo/TodoViewModel.kt
@@ -6,21 +6,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todoapp.data.repository.TodoRepository
+import com.example.todoapp.domain.models.Category
 import com.example.todoapp.domain.models.ToDo
 import com.example.todoapp.domain.usecases.AddTodoUseCase
 import com.example.todoapp.domain.usecases.ListTodosUseCase
 import com.example.todoapp.domain.usecases.SearchTodosUseCase
+import com.example.todoapp.domain.usecases.category.ListCategoryUseCase
 import kotlinx.coroutines.launch
 
 class TodoViewModel(
     private val todoRepository: TodoRepository,
     private val listTodosUseCase: ListTodosUseCase,
     private val addTodoUseCase: AddTodoUseCase,
-    private val searchTodosUseCase: SearchTodosUseCase
+    private val searchTodosUseCase: SearchTodosUseCase,
+    private val listCagetoriesUseCase: ListCategoryUseCase
 ) : ViewModel() {
 
     private val _todos = MutableLiveData<List<ToDo>>(emptyList())
     val todos: LiveData<List<ToDo>> = _todos
+    private val _categories = MutableLiveData<List<Category>>(emptyList())
+    val categories: LiveData<List<Category>> = _categories
 
 
     fun addTodo(todo: ToDo) {
@@ -76,6 +81,13 @@ class TodoViewModel(
         viewModelScope.launch {
             val todos = todoRepository.getAll()
             _todos.value = todos
+        }
+    }
+
+    fun getAllCategories() {
+        viewModelScope.launch {
+            val categories = listCagetoriesUseCase()
+            _categories.value = categories
         }
     }
 }
