effc4d82a6d34c6d746d27a2344bc8bb9c5aede0
jonathanduran7 <joniduran624@gmail.com>
2025-01-26 12:21:40 -0300

    feat(todo): modify get all method

--- app/src/main/java/com/example/todoapp/data/dao/TodoDao.kt
+++ app/src/main/java/com/example/todoapp/data/dao/TodoDao.kt
@@ -4,8 +4,10 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Update
 import com.example.todoapp.domain.models.ToDo
+import com.example.todoapp.domain.models.TodoWithCategory
 
 @Dao
 interface TodoDao {
@@ -18,9 +20,12 @@ interface TodoDao {
     @Query("DELETE FROM todo WHERE id = :todoId")
     suspend fun delete(todoId: Int)
 
+    @Transaction
     @Query("SELECT * FROM todo")
-    suspend fun getAll(): List<ToDo>
+    suspend fun getAll(): List<TodoWithCategory>
 
     @Query("SELECT * FROM todo where title like '%' || :query || '%'")
     suspend fun search(query: String): List<ToDo>
+
+
 }
