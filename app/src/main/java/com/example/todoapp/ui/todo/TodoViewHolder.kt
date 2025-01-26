f6efe4da4914d1e19bd2e48a7c45b5e2a0d397b2
jonathanduran7 <joniduran624@gmail.com>
2025-01-26 12:23:06 -0300

    feat(todo): replace todo to todoWithCategory in view holder

--- app/src/main/java/com/example/todoapp/ui/todo/TodoViewHolder.kt
+++ app/src/main/java/com/example/todoapp/ui/todo/TodoViewHolder.kt
@@ -3,19 +3,23 @@ package com.example.todoapp.ui.todo
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.R
 import com.example.todoapp.domain.models.ToDo
+import com.example.todoapp.domain.models.TodoWithCategory
 
 class TodoViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val checkBox: CheckBox = view.findViewById<CheckBox>(R.id.checkBox)
     private val deleteButton: Button = view.findViewById<Button>(R.id.deleteButton)
+    private val descriptionCategory: TextView = view.findViewById<TextView>(R.id.descriptionCategory)
 
-    fun bind(todo: ToDo, listener: OnTodoCheckListener, deleteListener: OnTodoDelete) {
+    fun bind(todo: TodoWithCategory, listener: OnTodoCheckListener, deleteListener: OnTodoDelete) {
         checkBox.setOnCheckedChangeListener(null)
 
-        checkBox.isChecked = todo.isCompleted
-        checkBox.text = todo.title
+        checkBox.isChecked = todo.todo.isCompleted
+        checkBox.text = todo.todo.title
+        descriptionCategory.text = todo.category?.name?: "No category"
 
         checkBox.setOnCheckedChangeListener { _, isChecked ->
             listener.onTodoCheckChanged(todo, isChecked)
