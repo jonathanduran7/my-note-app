1da417a3073c5f2c3ae4b445cd2971985f168d49
jonathanduran7 <joniduran624@gmail.com>
2025-01-26 12:20:25 -0300

    feat(todo): add new column for categoryId

--- app/src/main/java/com/example/todoapp/domain/models/ToDo.kt
+++ app/src/main/java/com/example/todoapp/domain/models/ToDo.kt
@@ -1,12 +1,25 @@
 package com.example.todoapp.domain.models
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "todo")
+@Entity(
+    tableName = "todo",
+    foreignKeys = [
+        ForeignKey(
+            entity = Category::class,
+            parentColumns = ["id"],
+            childColumns = ["categoryId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class ToDo(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
-    val isCompleted: Boolean
+    val isCompleted: Boolean,
+
+    val categoryId: Int?
 )
