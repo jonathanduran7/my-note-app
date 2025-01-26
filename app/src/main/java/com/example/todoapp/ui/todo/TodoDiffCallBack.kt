6590e36287edb588d593047f50beb53de98d1f1a
jonathanduran7 <joniduran624@gmail.com>
2025-01-26 12:22:36 -0300

    feat(todo): replace todo to todoWithCategory in diffcallback

--- app/src/main/java/com/example/todoapp/ui/todo/TodoDiffCallBack.kt
+++ app/src/main/java/com/example/todoapp/ui/todo/TodoDiffCallBack.kt
@@ -2,13 +2,14 @@ package com.example.todoapp.ui.todo
 
 import androidx.recyclerview.widget.DiffUtil
 import com.example.todoapp.domain.models.ToDo
+import com.example.todoapp.domain.models.TodoWithCategory
 
-class TodoDiffCallBack: DiffUtil.ItemCallback<ToDo>() {
-    override fun areItemsTheSame(oldItem: ToDo, newItem: ToDo): Boolean {
-        return oldItem.id == newItem.id
+class TodoDiffCallBack: DiffUtil.ItemCallback<TodoWithCategory>() {
+    override fun areItemsTheSame(oldItem: TodoWithCategory, newItem: TodoWithCategory): Boolean {
+        return oldItem.todo.id == newItem.todo.id
     }
 
-    override fun areContentsTheSame(oldItem: ToDo, newItem: ToDo): Boolean {
+    override fun areContentsTheSame(oldItem: TodoWithCategory, newItem: TodoWithCategory): Boolean {
         return oldItem == newItem
     }
 }
