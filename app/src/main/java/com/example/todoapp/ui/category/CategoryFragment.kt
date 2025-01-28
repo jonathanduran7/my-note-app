70d64c6d59a39be41e75e30cd98eeb50b66dbf94
jonathanduran7 <joniduran624@gmail.com>
2025-01-28 16:39:57 -0300

    feat(category): add alert dialog for delete category

--- app/src/main/java/com/example/todoapp/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/category/CategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.todoapp.ui.category
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -83,8 +84,7 @@ class CategoryFragment : Fragment(), OnCategoryDelete, OnCategoryEdit {
     }
 
     override fun onCategoryDelete(category: Category) {
-        viewModel.removeCategory(category)
-        categoryAdapter.notifyDataSetChanged()
+        showConfirmationDialog(category)
     }
 
     override fun onCategoryEdit(category: Category) {
@@ -112,6 +112,27 @@ class CategoryFragment : Fragment(), OnCategoryDelete, OnCategoryEdit {
             dialog.dismiss()
         }
 
+        dialog.show()
+    }
+
+    private fun showConfirmationDialog(
+        category: Category
+    ){
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Confirmacion")
+        builder.setMessage("¿Estas seguro de eliminar esta categoria? Todos los todos asociados a esta categoria seran eliminados.")
+
+        builder.setPositiveButton("Si") { dialog, which ->
+            viewModel.removeCategory(category)
+            categoryAdapter.notifyDataSetChanged()
+            dialog.dismiss()
+        }
+
+        builder.setNegativeButton("No") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        val dialog: AlertDialog = builder.create()
         dialog.show()
     }
 }
