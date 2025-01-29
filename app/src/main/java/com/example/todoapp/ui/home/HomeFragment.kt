68396dd026e9828987b7d06c61c3812a58bd6158
jonathanduran7 <joniduran624@gmail.com>
2025-01-28 23:05:12 -0300

    feat(home): implement recycler view and view model in fragment

--- app/src/main/java/com/example/todoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/home/HomeFragment.kt
@@ -1,31 +1,60 @@
 package com.example.todoapp.ui.home
 
-import androidx.fragment.app.viewModels
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.todoapp.R
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todoapp.databinding.FragmentHomeBinding
+import com.example.todoapp.domain.models.TodoWithCategory
+import com.example.todoapp.ui.todo.OnTodoCheckListener
+import com.example.todoapp.ui.todo.OnTodoDelete
+import org.koin.android.ext.android.inject
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnTodoCheckListener, OnTodoDelete {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
-    private val viewModel: HomeViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private val homeViewModel: HomeViewModel by inject()
+    private lateinit var homeAdapter : HomeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        homeAdapter = HomeAdapter(
+            this,
+            this
+        )
+
+        binding.recyclerViewHome.apply {
+            adapter = homeAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
+
+        homeViewModel.todos.observe(viewLifecycleOwner, Observer { todos ->
+            homeAdapter.submitList(todos)
+//            homeAdapter.notifyDataSetChanged()
+        })
+    }
+
+    override fun onTodoCheckChanged(todo: TodoWithCategory, isChecked: Boolean) {}
+
+    override fun onTodoDelete(todo: TodoWithCategory) {}
 }
