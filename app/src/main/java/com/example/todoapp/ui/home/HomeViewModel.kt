62eb1073bf97169dcbc7ddcf09d6dc3914e7f369
jonathanduran7 <joniduran624@gmail.com>
2025-01-28 23:04:34 -0300

    feat(home): add function for get all todos

--- app/src/main/java/com/example/todoapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/home/HomeViewModel.kt
@@ -1,7 +1,28 @@
 package com.example.todoapp.ui.home
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.domain.models.TodoWithCategory
+import com.example.todoapp.domain.usecases.GetRecentlyTodoUseCase
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(
+    private val listTodosUseCase: GetRecentlyTodoUseCase
+) : ViewModel() {
+    private val _todos = MutableLiveData<List<TodoWithCategory>>(emptyList())
+    val todos: LiveData<List<TodoWithCategory>> = _todos
+
+    private fun getAll() {
+        viewModelScope.launch {
+            _todos.value = listTodosUseCase()
+            Log.i("HomeViewModel", "getAll: ${_todos.value}")
+        }
+    }
+
+    init {
+        getAll()
+    }
 }
