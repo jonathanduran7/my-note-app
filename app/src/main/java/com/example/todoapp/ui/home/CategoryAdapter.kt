72e8c64f2d496e41f2f6ec3acbe270bc60062c99
jonathanduran7 <joniduran624@gmail.com>
2025-01-29 22:09:50 -0300

    feat(home): add category adapter

---
+++ app/src/main/java/com/example/todoapp/ui/home/CategoryAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.todoapp.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.todoapp.R
+import com.example.todoapp.domain.models.Category
+
+class CategoryAdapter: ListAdapter<Category, CategoryViewHolder>(CategoryDiffCallBack()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_category, parent, false)
+        return CategoryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
