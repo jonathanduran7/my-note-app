5877964eb96ce6f3bc5fe396a1cf7d80f7633d05
jonathanduran7 <joniduran624@gmail.com>
2025-01-29 22:09:58 -0300

    feat(home): add category diff callback

---
+++ app/src/main/java/com/example/todoapp/ui/home/CategoryDiffCallBack.kt
@@ -0,0 +1,14 @@
+package com.example.todoapp.ui.home
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.todoapp.domain.models.Category
+
+class CategoryDiffCallBack: DiffUtil.ItemCallback<Category>() {
+    override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem == newItem
+    }
+}
