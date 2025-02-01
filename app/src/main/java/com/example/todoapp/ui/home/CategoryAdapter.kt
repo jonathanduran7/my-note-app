d09e63fd961979f1299c231bd6b62bad240de201
jonathanduran7 <joniduran624@gmail.com>
2025-02-01 10:51:24 -0300

    feat(categoryDetail): implement interface listener in adapter

--- app/src/main/java/com/example/todoapp/ui/home/CategoryAdapter.kt
+++ app/src/main/java/com/example/todoapp/ui/home/CategoryAdapter.kt
@@ -6,13 +6,15 @@ import androidx.recyclerview.widget.ListAdapter
 import com.example.todoapp.R
 import com.example.todoapp.domain.models.Category
 
-class CategoryAdapter: ListAdapter<Category, CategoryViewHolder>(CategoryDiffCallBack()) {
+class CategoryAdapter(
+    private val listener: OnCategoryClick
+): ListAdapter<Category, CategoryViewHolder>(CategoryDiffCallBack()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.card_category, parent, false)
         return CategoryViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position), listener)
     }
 }
