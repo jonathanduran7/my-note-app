6a68da07a9c76732926fcc002a3d292dcdfed905
jonathanduran7 <joniduran624@gmail.com>
2025-02-01 12:52:01 -0300

    fix(categoryDetail): modify value in use case

--- app/src/main/java/com/example/todoapp/domain/usecases/GetTodoByCategoryUseCase.kt
+++ app/src/main/java/com/example/todoapp/domain/usecases/GetTodoByCategoryUseCase.kt
@@ -1,9 +1,12 @@
 package com.example.todoapp.domain.usecases
 
 import com.example.todoapp.data.repository.TodoRepository
+import com.example.todoapp.domain.models.TodoWithCategory
 
 class GetTodoByCategoryUseCase(
     private val todoRepository: TodoRepository
 ) {
-    suspend operator fun invoke(categoryId: Int) = todoRepository.getTodoByCategory(categoryId)
+    suspend operator fun invoke(categoryId: Int): List<TodoWithCategory> {
+        return todoRepository.getTodoByCategory(categoryId)
+    }
 }
