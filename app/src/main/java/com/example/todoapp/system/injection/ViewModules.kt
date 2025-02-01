fcaacfb9349ca7096a847e69bae6ef8bc63c60af
jonathanduran7 <joniduran624@gmail.com>
2025-02-01 12:53:14 -0300

    feat(categoryDetail): inject view model

--- app/src/main/java/com/example/todoapp/system/injection/ViewModules.kt
+++ app/src/main/java/com/example/todoapp/system/injection/ViewModules.kt
@@ -1,6 +1,7 @@
 package com.example.todoapp.system.injection
 
 import com.example.todoapp.ui.category.CategoryViewModel
+import com.example.todoapp.ui.categoryDetail.CategoryDetailViewModel
 import com.example.todoapp.ui.home.HomeViewModel
 import com.example.todoapp.ui.todo.TodoViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -10,4 +11,5 @@ val viewModels = module {
     viewModel { TodoViewModel(get(), get(), get(), get(), get()) }
     viewModel { CategoryViewModel(get(), get(), get(), get()) }
     viewModel { HomeViewModel(get(),get(), get(), get()) }
+    viewModel { CategoryDetailViewModel(get()) }
 }
