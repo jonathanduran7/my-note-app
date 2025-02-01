54343b472bf1182265db1e8e4d6c78c6378901f6
jonathanduran7 <joniduran624@gmail.com>
2025-02-01 12:52:25 -0300

    feat(categoryDetail): implement use case in view model

--- app/src/main/java/com/example/todoapp/ui/categoryDetail/CategoryDetailViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/categoryDetail/CategoryDetailViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.todoapp.ui.categoryDetail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.domain.models.TodoWithCategory
+import com.example.todoapp.domain.usecases.GetTodoByCategoryUseCase
+import kotlinx.coroutines.launch
 
-class CategoryDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class CategoryDetailViewModel(
+    private val getTodoByCategoryUseCase: GetTodoByCategoryUseCase
+) : ViewModel() {
+
+    private val _todos = MutableLiveData<List<TodoWithCategory>>(emptyList())
+    val todos: LiveData<List<TodoWithCategory>> = _todos
+
+        fun getTodoByCategory(categoryId: Int) {
+            viewModelScope.launch {
+                _todos.value = getTodoByCategoryUseCase(categoryId)
+            }
+        }
 }
