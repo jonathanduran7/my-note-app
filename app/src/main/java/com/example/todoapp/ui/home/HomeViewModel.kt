c35377ec7dbd48b028b66bd144ece7f645bf9a95
jonathanduran7 <joniduran624@gmail.com>
2025-01-31 21:23:53 -0300

    feat(home): get categories in view model

--- app/src/main/java/com/example/todoapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/home/HomeViewModel.kt
@@ -5,15 +5,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.todoapp.domain.models.Category
 import com.example.todoapp.domain.models.TodoWithCategory
 import com.example.todoapp.domain.usecases.GetRecentlyTodoUseCase
+import com.example.todoapp.domain.usecases.category.ListCategoryUseCase
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
-    private val listTodosUseCase: GetRecentlyTodoUseCase
+    private val listTodosUseCase: GetRecentlyTodoUseCase,
+    private val listCategoriesUseCase: ListCategoryUseCase
 ) : ViewModel() {
     private val _todos = MutableLiveData<List<TodoWithCategory>>()
     val todos: LiveData<List<TodoWithCategory>> = _todos
+
+    private val _categories = MutableLiveData<List<Category>>()
+    val categories: LiveData<List<Category>> = _categories
+
 
     private fun getAll() {
         viewModelScope.launch {
@@ -23,9 +30,16 @@ class HomeViewModel(
 
     init {
         getAll()
+        getCategories()
     }
 
     fun refreshTodos() {
         getAll()
+    }
+
+    private fun getCategories() {
+        viewModelScope.launch {
+            _categories.value = listCategoriesUseCase().toList()
+        }
     }
 }
